5832c75200004ccf64c9c8742db81fb50bb95315
arfenel <arfie.padilla@gmail.com>
2018-10-19 21:41:55 +0800

    Adapter

--- src/com/pattern/command/LightOnCommand.java
+++ src/com/pattern/command/LightOnCommand.java
@@ -1,14 +1,15 @@
 package com.pattern.command;
 
-
 //Concrete Command
-public class LightOnCommand implements ICommand{
-//reference to the light
-Light light;
-public LightOnCommand(Light light){
-  this.light = light;
-}
-public void execute(){
-  light.switchOn();
-}
+public class LightOnCommand implements ICommand {
+	// reference to the light
+	Light light;
+
+	public LightOnCommand(Light light) {
+		this.light = light;
+	}
+
+	public void execute() {
+		light.switchOn();
+	}
 }
